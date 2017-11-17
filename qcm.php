1321d3b91f3589ee3cb236901613857066f97f45
Xx-louloudu71-xX <louisguillotin.lg@gmail.com>
2017-11-17 12:19:27 +0100

    Class QCM
    Début de la classe QCM

---
+++ qcm.php
@@ -0,0 +1,23 @@
+<?php
+
+class Qcm
+{
+	private $_intitule;
+	private $_description;
+
+	public function __construct($inti, $desc){
+		$this->_intitule = $inti;
+		$this->_description = $desc;
+	}
+
+	public function addQuestion(){
+
+	}
+
+	public function displayQCM(){
+		echo "<p>".$this->_intitule."</p><br/><p>".$this->_description."</p>";
+	}
+
+}
+
+?>
