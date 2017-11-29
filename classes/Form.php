82ba38566bb647b22894cd6ada7501ce1573d285
Xx-louloudu71-xX <louisguillotin.lg@gmail.com>
2017-11-29 14:25:44 +0100

    Modification Classe Form
    Permet d'ajouter un attribut classe au Form et au Input pour les utiliser dans le CSS.

--- classes/Form.php
+++ classes/Form.php
@@ -7,10 +7,11 @@
 		 *	@param string $_name le nom du formulaire créé
 		 *	@param string $_action l'action du formulaire créé
 		 *	@param string $_method la méthode utilisée dans ce formulaire
+		 *  @param string $_class la class à utilisé dans le CSS
 		 *	@param string $_enctype l'enctype utlisé dans ce formulaire
 		*/
-		public function __construct($_name,$_action,$_method,$_enctype){
-			$this->form = "<form name='".$_name."' action='".$_action."' method='".$_method."'";
+		public function __construct($_class,$_name,$_action,$_method,$_enctype){
+			$this->form = "<form class='".$_class."' name='".$_name."' action='".$_action."' method='".$_method."'";
 
 			//On regarde si on a un 'enctype'
 			if($_enctype == ""){
@@ -25,11 +26,12 @@
 		 * 	Permet d'ajouter un input dans le formulaire créé
 		 *	@param string $_type le type de l'input ajouté
 		 *	@param string $_name le nom de l'input ajouté
+		 *  @param string $_class la class à utilisé dans le CSS
 		 *	@param string $_placeholder le placeholder de l'input ajouté (peut être vide)
 		 *	@param boolean $_required si oui ou non l'input est obligatoire à remplir
 		*/
-		public function set_input($_type,$_name,$_placeholder,$_required){
-			$this->form .= "<input type='".$_type."'";
+		public function set_input($_class, $_type,$_name,$_placeholder,$_required){
+			$this->form .= "<input class ='".$_class."' type='".$_type."'";
 
 			if($_name != ""){
 				$this->form .= " name='".$_name."'";
@@ -39,7 +41,7 @@
 				$this->form .= " placeholder='".$_placeholder."'";
 			}
 
-			if($_required){
+			if($_required == "true"){
 				$this->form .= " required";
 			}
 
@@ -48,11 +50,12 @@
 
 		/**
 		 * 	Permet d'ajouter le bouton de validation du formulaire
+		 *	@param string $_class la classe utilisé dans le CSS
 		 *	@param string $_name le nom du bouton de validation
 		 *	@param string $_value la valeur que prend le bouton de validation
 		*/
-		public function set_submit($_name,$_value){
-			$this->form .= "<input type='submit' name='".$_name."' value='".$_value."'>";
+		public function set_submit($_class, $_name,$_value){
+			$this->form .= "<input class='".$_class."' type='submit' name='".$_name."' value='".$_value."'>";
 		}
 
 		/**
@@ -74,14 +77,15 @@
 		}
 		/**
 		 * 	Permet d'ajouter un input avec une valeur dans le formulaire créé
+		 *  @param string $_class la class utilisé dans le CSS
 		 *	@param string $_type le type de l'input ajouté
 		 *	@param string $_name le nom de l'input ajouté
 		 *	@param string $_placeholder le placeholder de l'input ajouté (peut être vide)
 		 *	@param boolean $_required si oui ou non l'input est obligatoire à remplir
 		 *	@param string $_value la valeur que l'input prend
 		*/
-		public function set_input_value($_type,$_name,$_placeholder,$_required,$_value){
-			$this->form .= "<input type='".$_type."'";
+		public function set_input_value($_class, $_type,$_name,$_placeholder,$_required,$_value){
+			$this->form .= "<input class ='".$_class."' type='".$_type."'";
 
 			if($_name != ""){
 				$this->form .= " name='".$_name."'";
