f95c11f433fe58e9c45bd4cd50bd3119ee0d3358
Khant1 <kant1.berton@gmail.com>
2017-11-29 14:42:06 +0100

    index et html

--- classes/html.php
+++ classes/html.php
@@ -7,27 +7,34 @@ class HTML{
 
 	public function __construct($_logo,$_charset,$_titre,$_css){
 		$this->logo = $_logo;
+		echo $this->logo;
 		$this->charset = $_charset;
+		echo $this->charset;
 		$this->titre = $_titre;
+		echo $this->titre;
 		$this->css = $_css;
+		echo $this->css;
 	}
 
 	public function meta_data(){
-		if(!is_null($this->logo) && !isset($this->logo)){
-			echo '<link rel="shortcut icon" type="image/x-icon" href="' . $this->logo . '">';
+		$code = "";
+		if(!is_null($this->logo) && isset($this->logo)){
+			$code .= '<link rel="shortcut icon" type="image/x-icon" href="' . $this->logo . '">';
 		}
 
-		if(!is_null($this->charset) && !isset($this->charset)){
-			echo '<meta charset="' . $this->charset . '">';
+		if(!is_null($this->charset) && isset($this->charset)){
+			$code .= '<meta charset="' . $this->charset . '">';
 		}
 
-		if(!is_null($this->titre) && !isset($this->titre)){
-			echo '<title>' . $this->titre . '</title>';
+		if(!is_null($this->titre) && isset($this->titre)){
+			$code .= '<title>' . $this->titre . '</title>';
 		}
 
-		if(!is_null($this->css) && !isset($this->css)){
-			echo '<link rel="stylesheet" href="../style/' . $this->css . '">';
+		if(!is_null($this->css) && isset($this->css)){
+			$code .= '<link rel="stylesheet" href="../style/' . $this->css . '">';
 		}
+
+		return $code;
 	}
 }
 ?>
--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <?php
-	session_start();
+session_start();
 
-	require_once("classes/form.php");
+require_once("classes/form.php");
+include("classes/HTML.php");
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>QCM</title>
+	<?php
+	$html = new HTML(null,"utf-8","QCM","style.css");
+	echo $html->meta_data();
+	?>
 </head>
 <body>
 	<?php
-		$form_connexion = new form("connexion","form_connexion.php","post","");
-		$form_connexion->set_input("text","id","Login",1);
-		$form_connexion->set_input("password","mdp","Mot de passe",1);
-		$form_connexion->set_submit("validerconnexion","Connexion");
-		$form_connexion->get_form();
+	$form_connexion = new form("connexion","form_connexion.php","post","");
+	$form_connexion->set_input("text","id","Login",1);
+	$form_connexion->set_input("password","mdp","Mot de passe",1);
+	$form_connexion->set_submit("validerconnexion","Connexion");
+	$form_connexion->get_form();
 	?>
 </body>
 </html>
