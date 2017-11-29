ce61d34ae521041367ca5677154f63a0e8f838c3
Xx-louloudu71-xX <louisguillotin.lg@gmail.com>
2017-11-29 14:10:49 +0100

    QCM
    QCM

--- classes/qcm.php
+++ classes/qcm.php
@@ -2,20 +2,21 @@
 
 class Qcm
 {
-	private $_intitule;
-	private $_description;
+	private $intitule;
+	private $description;
+	private $note;
 
 	public function __construct($inti, $desc){
-		$this->_intitule = $inti;
-		$this->_description = $desc;
+		$this->intitule = $inti;
+		$this->description = $desc;
 	}
 
 	public function addQuestion(){
-		
+		echo '<input id="" type="button"'
 	}
 
 	public function displayQCM(){
-		echo "<p>".$this->_intitule."</p><br/><p>".$this->_description."</p>";
+		echo "<p>".$this->intitule."</p><br/><p>".$this->description."</p>";
 	}
 
 }
