a441d2f1cb3f9c4032fea49c437d5052d8b242e4
BREVET MAEL <mael.brevet@gmail.com>
2017-12-04 11:57:54 +0100

    change

--- classes/BaseDeDonnees.php
+++ classes/BaseDeDonnees.php
@@ -70,6 +70,7 @@
 				$dsn = 'mysql:host='.$this->host.';port=3306;dbname='.$this->bdName.'';
 				$this->pdo = new PDO($dsn, $this->user, $this->password,
 				array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+				$this->pdo->setAttribut(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}
 			catch (Exception $e){
 				die('Erreur : ' . $e->getMessage());
--- pageEnseignant.php
+++ pageEnseignant.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	require_once("/fonctions/connexion_bdd.php");
+	require_once("fonctions/connexion_bdd.php");
 
 	//On vérifie que l'utilisateur est connecté et qu'il s'agit bien d'un enseignant
 	if(!isset($_SESSION['utilisateur']) || !is_a($_SESSION['utilisateur'],"Enseignant")){
