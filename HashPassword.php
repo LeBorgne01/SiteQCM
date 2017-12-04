4d1c5c158b4d10f1eeabd6360ecd130f12e2036b
DANJOUX MICKAEL p1602272 <mickael.danjoux@etu.univ-lyon1.fr>
2017-12-04 12:01:25 +0100

    hash

---
+++ HashPassword.php
@@ -0,0 +1,20 @@
+<?php 
+	
+session_start();
+	require_once("fonctions/connexion_bdd.php");
+	require_once("classes/Form.php");
+		$form = new form("connexion","HashPassword.php","post","");
+		$form->set_input("text","password","Password",1);
+		$form->add_br();
+		$form->set_submit("validerHash","Hashage");
+		echo $form->get_form();
+		
+		
+ if(!empty($_POST['password'])){
+
+ 	$hash = hash_password($_POST['password']);
+ 	echo $hash;
+
+ }
+
+ ?>
